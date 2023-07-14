6b1386d0f88abc1473bb653fb34d654e514d7476
Vanja Vego <vanja.vego@gmail.com>
2023-07-14 09:27:24 -0400

    Added models and set up their relationships

---
+++ Ministack/Migrations/20230714041123_AddedTables.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ministack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    startDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    endDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ScheduleId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Event_Schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventLocation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AddressLine1 = table.Column<string>(type: "TEXT", nullable: false),
+                    AddressLine2 = table.Column<string>(type: "TEXT", nullable: false),
+                    Country = table.Column<string>(type: "TEXT", nullable: false),
+                    CountryCode = table.Column<string>(type: "TEXT", nullable: true),
+                    State = table.Column<string>(type: "TEXT", nullable: true),
+                    Province = table.Column<string>(type: "TEXT", nullable: true),
+                    Region = table.Column<string>(type: "TEXT", nullable: true),
+                    PostalCode = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    EventRef = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventLocation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventLocation_Event_EventRef",
+                        column: x => x.EventRef,
+                        principalTable: "Event",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_ScheduleId",
+                table: "Event",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventLocation_EventRef",
+                table: "EventLocation",
+                column: "EventRef",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventLocation");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
--- Ministack/Models/Model.cs
+++ Ministack/Models/Model.cs
@@ -20,16 +20,76 @@ public class MiniContext : DbContext
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Schedule>()
+            .HasMany(c => c.Events)
+            .WithOne(e => e.Schedule)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Event>()
+            .HasOne(a => a.Location)
+            .WithOne(b => b.Event)
+            .HasForeignKey<EventLocation>(b => b.EventRef)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
 public class Schedule
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
+
+    public ICollection<Event>? Events { get; set; }
 }
 
 public class Response
 {
     public string Status { get; set; }
     public string Message { get; set; }
+}
+
+public class Event 
+{ 
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime startDateTime { get; set; }
+
+    public DateTime endDateTime { get; set; }
+
+    public EventLocation? Location { get; set; }
+
+    public Schedule Schedule { get; set; }
+
+}
+
+public class EventLocation
+{
+    public int Id { get; set; }
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set;}
+
+    public string Country { get; set; }
+    public string? CountryCode { get; set; }
+
+    public string? State { get; set; }
+    public string? Province { get; set; }
+    public string? Region { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public string City { get; set;}
+
+    public string Phone { get; set;}
+
+    public Event Event { get; set; }
+
+    public int EventRef { get; set; }
+
 }
